ff3e1535ea3d2ab124bd3bb0a007d022ae2c57c5
ultimulti <manhtusuperman@gmail.com>
2024-03-28 23:35:29 -0700

    changes

--- src/services/generateFlashcards.js
+++ src/services/generateFlashcards.js
@@ -2,7 +2,7 @@
 
 const generateFlashcards = async (inpText) => {
     const OpenAi = require('openai');
-    // const openai = new OpenAi({ apiKey:  process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
+    const openai = new OpenAi({ apiKey:  process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
     
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
